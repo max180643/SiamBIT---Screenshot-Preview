4df760e333edc373dc1dae0f908233da1585bfa1
Chanwit Settavongsin <max180643@gmail.com>
2019-05-26 15:17:59 +0700

    Add Code 💾

---
+++ SiamBIT - Screenshot Preview/content.js
@@ -0,0 +1,10 @@
+var list = document.querySelectorAll('[title="รูปภาพตัวอย่าง"]');
+    for (i = 0; i < list.length; i++) {
+        var extension = list[i].parentNode.href.split('.').pop();
+        if (extension === 'jpg' || extension === 'jpeg' || extension === 'png' || extension === 'gif') {
+            list[i].src = list[i].parentNode.href;
+            list[i].height = 100;
+            list[i].align = "top";
+            list[i].style.display = "block";
+        }
+    }
